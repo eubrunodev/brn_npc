4ebc18727d580d0670e5de406bc2d5fb8b8343df
eubrunodev <bruno.peniani03@gmail.com>
2023-09-14 22:50:39 -0300

    first up

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+
+Config.locais = {
+    -- SIGA OS EXEMPLOS ABAIXO, DE COMO COLOCAR UM NPC
+
+    -- coordenada x -- coordenada y -- coordenada z -- coordenada rotação -- esse pedModel é o tipo do NPC, tem um site aonde você consegue verificar
+    { x = 1254.65, y = -226.48, z = 99.01, rot = 94.2, pedModel = GetHashKey("ig_claypain") },
+}
+
+
+Config.infoBarMessage = "PRESSIONE ~g~E~s~ PARA INICIAR A ROTA" -- coloque a mensagem que você quiser
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "adamant"
+game "gta5"
+
+author "ensaboadorp"
+description "A simple FiveM script to spawn NPC's"
+version "1.0"
+
+client_scripts {
+    "client.lua",
+}
